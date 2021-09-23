37310a23efdd7dd7a87d3c47ba06687129fc3f40
wangchao <wangchao@ipfsforce.com>
2021-09-23 14:51:37 +0800

    deal infos

--- racailum/segment/extract/actorstate/deal_proposal_detail_v5.go
+++ racailum/segment/extract/actorstate/deal_proposal_detail_v5.go
@@ -47,6 +47,7 @@ func extractDealProposalDetailedV5(ctx *extract.Ctx, res *extract.Res, head *com
 	details := map[address.Address]*model.DealProposalDetail{}
 
 	var out market5.DealProposal
+	var dealProposals []model.DealProposal
 	err = deals.ForEach(&out, func(idx int64) error {
 		if _, ok := details[out.Provider]; !ok {
 			details[out.Provider] = &model.DealProposalDetail{
@@ -75,6 +76,12 @@ func extractDealProposalDetailedV5(ctx *extract.Ctx, res *extract.Res, head *com
 		} else {
 			details[out.Provider].Detail.UnVerifiedDealEndCount++
 		}
+
+		dealProposals = append(dealProposals, model.DealProposal{
+			ID:           idx,
+			Epoch:        head.Epoch,
+			DealProposal: out,
+		})
 		return nil
 	})
 
@@ -86,5 +93,8 @@ func extractDealProposalDetailedV5(ctx *extract.Ctx, res *extract.Res, head *com
 		res.Docs = append(res.Docs, details[i])
 	}
 
+	for i := range dealProposals {
+		res.Docs = append(res.Docs, &dealProposals[i])
+	}
 	return nil
 }
