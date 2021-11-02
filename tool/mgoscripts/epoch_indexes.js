3cc8123842e0bd39fed594a715fea116cac5920b
wangchao <wangchao@ipfsforce.com>
2021-11-02 11:06:48 +0800

    gen index

--- tool/mgoscripts/epoch_indexes.js
+++ tool/mgoscripts/epoch_indexes.js
@@ -1,8 +1,21 @@
+db.ActorBalance.createIndex({"Addresses":1}, {"sparse": true});
+db.ActorBalance.createIndex({"Code":1}, {"sparse": true});
+db.ActorBalance.createIndex({"Addresses":1,"Code":1}, {"sparse": true});
+
+db.ActorState.createIndex({"Epoch":1,"Code":1,"Addr":1}, {"sparse": true});
+
 db.AllocatedSectors.createIndex({"Epoch":1,"Addr":1}, {"sparse": true});
 
 db.BlockHeader.createIndex({"Epoch":1,"Miner":1}, {"sparse": true});
 
 db.ClaimedPower.createIndex({"Epoch":1,"Addr":1}, {"sparse": true});
+
+db.DealProposal.createIndex({"Epoch":1,"VerifiedDeal":1}, {"sparse": true});
+db.DealProposal.createIndex({"VerifiedDeal":1}, {"sparse": true});
+db.DealProposal.createIndex({"Provider":1}, {"sparse": true});
+db.DealProposal.createIndex({"Client":1}, {"sparse": true});
+
+db.DealProposalDetail.createIndex({"Epoch":1,"Addr":1}, {"sparse": true});
 
 db.DealProposalSummary.createIndex({"Epoch":1,"Addr":1}, {"sparse": true});
 
@@ -15,10 +28,15 @@ db.ExecTrace.createIndex({"Cid":1}, {"sparse": true});
 
 // no indexes for FilSupply
 
+db.MarketFunds.createIndex({"Epoch":1,"Addr":1}, {"sparse": true});
+
 db.Message.createIndex({"From":1,"Nonce":1}, {"sparse": true});
 db.Message.createIndex({"To":1,"Method":1}, {"sparse": true});
 db.Message.createIndex({"Detail.Method":1,"Detail.Actor":1}, {"sparse": true});
-db.Message.createIndex({"Detail.PackedHeight":1},{"sparse":true});
+db.Message.createIndex({"Detail.PackedHeight":1}, {"sparse": true});
+db.Message.createIndex({"Detail.PackedHeight":1,"Detail.Method":1}, {"sparse": true});
+
+db.MinerDealSector.createIndex({"Epoch":1,"Miner":1}, {"sparse": true});
 
 db.MinerFunds.createIndex({"Epoch":1,"Addr":1}, {"sparse": true});
 
