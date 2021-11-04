4d4b0d9966426dd23b0fe6ec730a84c6bd104ea9
dtynn <dtynn@163.com>
2021-11-04 13:26:36 +0800

    chore: add echo line in check-gens.sh

--- tool/scripts/check-gens.sh
+++ tool/scripts/check-gens.sh
@@ -5,8 +5,8 @@ make gen-index
 make gen-model
 make gen-diff
 
+echo "check for file changes"
 commit=$(git describe --always --match=NeVeRmAtCh --dirty)
-
 if [[ $commit == *"-dirty" ]]; then
 	echo "un-committed changes"
 	exit 1
