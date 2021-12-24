2856d0b36ddc3a52566137ddd8e2f221584cb55a
liubin <nibuil0730@163.com>
2021-12-24 15:52:31 +0800

    单元测试 miner_founds_v6 && miner_sector_health_v6

---
+++ racailum/segment/extract/actorstate/gen/miner_funds_v6_test.go
@@ -0,0 +1,40 @@
+package gen
+import (
+	"context"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/lotus/chain/store"
+	"github.com/filecoin-project/lotus/chain/types"
+	miner6 "github.com/filecoin-project/specs-actors/v6/actors/builtin/miner"
+	"github.com/ipfs-force-community/londobell/common"
+	"github.com/ipfs-force-community/londobell/racailum/segment/actor"
+	"github.com/ipfs-force-community/londobell/racailum/segment/extract"
+	"github.com/ipfs-force-community/londobell/testutils"
+	"github.com/ipfs/go-cid"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+	"testing"
+)
+
+func Test_extractMinerFundsV6(t *testing.T) {
+	ctx := context.Background()
+	res := extract.NewRes(0, 0)
+	localBs, closer,err:= testutils.NewLocalBlockStore(ctx)
+	require.NoError(t, err)
+	defer func() {
+		_ = closer()
+	}()
+	headerCId, _ := cid.Decode("bafy2bzacedjnglismll3py5nx6q6g2lha6xwovrncl6b53mrx5v6m4k4wuc6y")
+	actorStore := store.ActorStore(ctx,localBs)
+	var out miner6.State
+	err = actorStore.Get(ctx,headerCId,&out)
+	require.NoError(t, err)
+	mockDAL := &MockDAL{}
+	mockDAL.On("ActorStore", ctx).Return(store.ActorStore(ctx, localBs), nil)
+	ectx, err := extract.NewCtx(ctx, mockDAL, &zap.SugaredLogger{}, &actor.Set{}, extract.DryOptions())
+	err = extractMinerFundsV6(ectx,res,&common.ActorHead{
+		Actor: &types.Actor{Head: headerCId},
+		Epoch: abi.ChainEpoch(455000),
+	},&out)
+
+	require.NoError(t, err)
+}
