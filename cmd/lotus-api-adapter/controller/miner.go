224dd0d34340089499451978285b97bc2c60b4b9
周琳 <zhoulin@zhoulindeMacBook-Pro.local>
2022-06-30 17:59:48 +0800

    feat: fix addr & upgrade lotusv1.16.0 for miner api

--- cmd/lotus-api-adapter/controller/miner.go
+++ cmd/lotus-api-adapter/controller/miner.go
@@ -6,11 +6,6 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/actors/builtin"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
-	"github.com/filecoin-project/lotus/chain/store"
-	"github.com/filecoin-project/lotus/chain/types"
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
@@ -19,16 +14,29 @@ import (
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	miner3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/miner"
+	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	miner4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/miner"
+	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
 	builtin5 "github.com/filecoin-project/specs-actors/v5/actors/builtin"
 	miner5 "github.com/filecoin-project/specs-actors/v5/actors/builtin/miner"
+	adt5 "github.com/filecoin-project/specs-actors/v5/actors/util/adt"
 	builtin6 "github.com/filecoin-project/specs-actors/v6/actors/builtin"
 	miner6 "github.com/filecoin-project/specs-actors/v6/actors/builtin/miner"
+	adt6 "github.com/filecoin-project/specs-actors/v6/actors/util/adt"
 	builtin7 "github.com/filecoin-project/specs-actors/v7/actors/builtin"
 	miner7 "github.com/filecoin-project/specs-actors/v7/actors/builtin/miner"
-	"github.com/filecoin-project/specs-actors/v7/actors/util/adt"
+	adt7 "github.com/filecoin-project/specs-actors/v7/actors/util/adt"
+	builtin8 "github.com/filecoin-project/specs-actors/v8/actors/builtin"
+	miner8 "github.com/filecoin-project/specs-actors/v8/actors/builtin/miner"
+	adt8 "github.com/filecoin-project/specs-actors/v8/actors/util/adt"
 	"github.com/gin-gonic/gin"
+
+	"github.com/filecoin-project/lotus/blockstore"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
+	"github.com/filecoin-project/lotus/chain/store"
+	"github.com/filecoin-project/lotus/chain/types"
 
 	"github.com/ipfs-force-community/londobell/cmd/lotus-api-adapter/model"
 )
@@ -411,7 +419,7 @@ func GetMinerInfo(c *gin.Context) {
 			return
 		}
 
-		precommitted, err := adt.AsMap(stor, state.PreCommittedSectors, builtin3.DefaultHamtBitwidth)
+		precommitted, err := adt3.AsMap(stor, state.PreCommittedSectors, builtin3.DefaultHamtBitwidth)
 		if err != nil {
 			res.Code = model.Fail
 			res.Msg = err.Error()
@@ -505,7 +513,7 @@ func GetMinerInfo(c *gin.Context) {
 			return
 		}
 
-		precommitted, err := adt.AsMap(stor, state.PreCommittedSectors, builtin4.DefaultHamtBitwidth)
+		precommitted, err := adt4.AsMap(stor, state.PreCommittedSectors, builtin4.DefaultHamtBitwidth)
 		if err != nil {
 			res.Code = model.Fail
 			res.Msg = err.Error()
@@ -599,7 +607,7 @@ func GetMinerInfo(c *gin.Context) {
 			return
 		}
 
-		precommitted, err := adt.AsMap(stor, state.PreCommittedSectors, builtin5.DefaultHamtBitwidth)
+		precommitted, err := adt5.AsMap(stor, state.PreCommittedSectors, builtin5.DefaultHamtBitwidth)
 		if err != nil {
 			res.Code = model.Fail
 			res.Msg = err.Error()
@@ -692,7 +700,7 @@ func GetMinerInfo(c *gin.Context) {
 			return
 		}
 
-		precommitted, err := adt.AsMap(stor, state.PreCommittedSectors, builtin6.DefaultHamtBitwidth)
+		precommitted, err := adt6.AsMap(stor, state.PreCommittedSectors, builtin6.DefaultHamtBitwidth)
 		if err != nil {
 			res.Code = model.Fail
 			res.Msg = err.Error()
@@ -786,7 +794,7 @@ func GetMinerInfo(c *gin.Context) {
 			return
 		}
 
-		precommitted, err := adt.AsMap(stor, state.PreCommittedSectors, builtin7.DefaultHamtBitwidth)
+		precommitted, err := adt7.AsMap(stor, state.PreCommittedSectors, builtin7.DefaultHamtBitwidth)
 		if err != nil {
 			res.Code = model.Fail
 			res.Msg = err.Error()
@@ -801,163 +809,102 @@ func GetMinerInfo(c *gin.Context) {
 		})
 
 		resData.State = state
+
+	case builtin8.StorageMinerActorCodeID:
+		state := miner8.State{}
+		err = stor.Get(ctx, mact.Head, &state)
+		if err != nil {
+			res.Code = model.Fail
+			res.Msg = err.Error()
+			c.JSON(http.StatusOK, res)
+			return
+		}
+
+		dls, err := state.LoadDeadlines(stor)
+		if err != nil {
+			res.Code = model.Fail
+			res.Msg = err.Error()
+			c.JSON(http.StatusOK, res)
+			return
+		}
+
+		err = dls.ForEach(stor, func(dlIdx uint64, dl *miner8.Deadline) error {
+			partitions, err := dl.PartitionsArray(stor)
+			if err != nil {
+				return err
+			}
+			var part miner8.Partition
+			return partitions.ForEach(&part, func(partIdx int64) error {
+				sc, err := part.Sectors.Count()
+				if err != nil {
+					return err
+				}
+				sectorCount += sc
+
+				fc, err := part.Faults.Count()
+				if err != nil {
+					return err
+				}
+				faultSectorCount += fc
+
+				active, err := part.ActiveSectors()
+				if err != nil {
+					return err
+				}
+				ac, err := active.Count()
+				if err != nil {
+					return err
+				}
+				activeSectorCount += ac
+
+				live, err := part.LiveSectors()
+				if err != nil {
+					return err
+				}
+				lc, err := live.Count()
+				if err != nil {
+					return err
+				}
+				liveSectorCount += lc
+
+				rc, err := part.Recoveries.Count()
+				if err != nil {
+					return err
+				}
+				recoverSectorCount += rc
+
+				tc, err := part.Terminated.Count()
+				if err != nil {
+					return err
+				}
+				terminateSectorCount += tc
+
+				return nil
+			})
+		})
+		if err != nil {
+			res.Code = model.Fail
+			res.Msg = err.Error()
+			c.JSON(http.StatusOK, res)
+			return
+		}
+
+		precommitted, err := adt8.AsMap(stor, state.PreCommittedSectors, builtin8.DefaultHamtBitwidth)
+		if err != nil {
+			res.Code = model.Fail
+			res.Msg = err.Error()
+			c.JSON(http.StatusOK, res)
+			return
+		}
+
+		var precommit miner8.SectorPreCommitOnChainInfo
+		precommitted.ForEach(&precommit, func(string) error { // nolint: errcheck
+			precommitSectorCount++
+			return nil
+		})
+
+		resData.State = state
 	}
-
-	//allSectors, err := miner.AllPartSectors(mas, miner.Partition.ActiveSectors)
-	//if err != nil {
-	//	res.Code = model.Fail
-	//	res.Msg = err.Error()
-	//	c.JSON(http.StatusOK, res)
-	//	return
-	//}
-	//
-	//sectorCount, err = allSectors.Count()
-	//if err != nil {
-	//	res.Code = model.Fail
-	//	res.Msg = err.Error()
-	//	c.JSON(http.StatusOK, res)
-	//	return
-	//}
-	//
-	//faultySectors, err := miner.AllPartSectors(mas, miner.Partition.FaultySectors)
-	//if err != nil {
-	//	res.Code = model.Fail
-	//	res.Msg = err.Error()
-	//	c.JSON(http.StatusOK, res)
-	//	return
-	//}
-	//
-	//faultSectorCount, err = faultySectors.Count()
-	//if err != nil {
-	//	res.Code = model.Fail
-	//	res.Msg = err.Error()
-	//	c.JSON(http.StatusOK, res)
-	//	return
-	//}
-	//
-	//activeSectors, err := miner.AllPartSectors(mas, miner.Partition.ActiveSectors) //todo:network版本？
-	//if err != nil {
-	//	res.Code = model.Fail
-	//	res.Msg = err.Error()
-	//	c.JSON(http.StatusOK, res)
-	//	return
-	//}
-	//
-	//activeSectorCount, err = activeSectors.Count()
-	//if err != nil {
-	//	res.Code = model.Fail
-	//	res.Msg = err.Error()
-	//	c.JSON(http.StatusOK, res)
-	//	return
-	//}
-	//
-	//liveSectors, err := miner.AllPartSectors(mas, miner.Partition.LiveSectors)
-	//if err != nil {
-	//	res.Code = model.Fail
-	//	res.Msg = err.Error()
-	//	c.JSON(http.StatusOK, res)
-	//	return
-	//}
-	//
-	//liveSectorCount, err = liveSectors.Count()
-	//if err != nil {
-	//	res.Code = model.Fail
-	//	res.Msg = err.Error()
-	//	c.JSON(http.StatusOK, res)
-	//	return
-	//}
-	//
-	//recoverSectors, err := miner.AllPartSectors(mas, miner.Partition.RecoveringSectors)
-	//if err != nil {
-	//	res.Code = model.Fail
-	//	res.Msg = err.Error()
-	//	c.JSON(http.StatusOK, res)
-	//	return
-	//}
-	//
-	//recoverSectorCount, err = recoverSectors.Count()
-	//if err != nil {
-	//	res.Code = model.Fail
-	//	res.Msg = err.Error()
-	//	c.JSON(http.StatusOK, res)
-	//	return
-	//}
-	//
-	//terminateSectorCount
-
-	//if err = mas.ForEachDeadline(func(idx uint64, dl miner.Deadline) error {
-	//	return dl.ForEachPartition(func(partidx uint64, part miner.Partition) error {
-	//		allSectors, err := part.AllSectors()
-	//		if err != nil {
-	//			return err
-	//		}
-	//
-	//		sc, err := allSectors.Count()
-	//		if err != nil {
-	//			return err
-	//		}
-	//		sectorCount += sc
-	//
-	//		faultSectors, err := part.FaultySectors()
-	//		if err != nil {
-	//			return err
-	//		}
-	//
-	//		fc, err := faultSectors.Count()
-	//		if err != nil {
-	//			return err
-	//		}
-	//		faultSectorCount += fc
-	//
-	//		activeSectors, err := part.ActiveSectors()
-	//		if err != nil {
-	//			return err
-	//		}
-	//
-	//		ac, err := activeSectors.Count()
-	//		if err != nil {
-	//			return err
-	//		}
-	//		activeSectorCount += ac
-	//
-	//		liveSectors, err := part.LiveSectors()
-	//		if err != nil {
-	//			return err
-	//		}
-	//
-	//		lc, err := liveSectors.Count()
-	//		if err != nil {
-	//			return err
-	//		}
-	//		liveSectorCount += lc
-	//
-	//		recoverSectors, err := part.RecoveringSectors()
-	//		if err != nil {
-	//			return err
-	//		}
-	//
-	//		rc, err := recoverSectors.Count()
-	//		if err != nil {
-	//			return err
-	//		}
-	//		recoverSectorCount += rc
-	//
-	//		unprovenSectors, err := part.UnprovenSectors()
-	//		if err != nil {
-	//			return err
-	//		}
-	//
-	//		uc, err := unprovenSectors.Count()
-	//		if err != nil {
-	//			return err
-	//		}
-	//
-	//		tc := sc - ac - fc - uc
-	//		terminateSectorCount += tc
-	//
-	//		return nil
-	//	})
-	//})
 
 	resData.Epoch = ts.Height()
 	resData.Miner = maddr
@@ -970,7 +917,7 @@ func GetMinerInfo(c *gin.Context) {
 	resData.Balance = mact.Balance
 	resData.AvailableBalance = availableBalance
 	resData.VestingFunds = lockedFunds.VestingFunds
-	resData.LockedFunds = lockedFunds.PreCommitDeposits //todo:不算init_pledge?
+	resData.LockedFunds = lockedFunds.PreCommitDeposits
 	resData.InitialPledgeRequirement = lockedFunds.InitialPledgeRequirement
 	resData.SectorCount = sectorCount
 	resData.FaultSectorCount = faultSectorCount
