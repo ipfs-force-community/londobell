0a68df0affaf0fcc0a64e5a2adf8b845059fc315
周琳 <zhoulin@zhoulindeMacBook-Pro.local>
2022-08-23 13:34:52 +0800

    add access_token

--- cmd/londobell-api/controller/aggregators/aggregators.go
+++ cmd/londobell-api/controller/aggregators/aggregators.go
@@ -1,8 +1,7 @@
 package aggregators
 
-import pool_monitor "github.com/ipfs-force-community/londobell-aggregators/pool-monitor"
+import monitor "github.com/ipfs-force-community/londobell-aggregators/pool-monitor"
 
-// todo: 确认路径
 var (
 	addressAggregator          []byte
 	aggPreNetfeeAggregator     []byte
@@ -19,15 +18,15 @@ var (
 
 func InitAggregators() {
 	// todo: 定期重新读取，无感知变化 or 每次变化重启
-	addressAggregator = pool_monitor.GetAddressAggregator()
-	aggPreNetfeeAggregator = pool_monitor.GetAggPreNetfeeAggregator()
-	aggProNetfeeAggregator = pool_monitor.GetAggProNetfeeAggregator()
-	blockAggregator = pool_monitor.GetBlockAggregator()
-	finalHeightAggregator = pool_monitor.GetFinalHeightAggregator()
-	minerBlockrewardAggregator = pool_monitor.GetMinerBlockrewardAggregator()
-	minersInfoAggregator = pool_monitor.GetMinersInfoAggregator()
-	minersMinedAggregator = pool_monitor.GetMinersMinedAggregator()
-	multisigMessageAggregator = pool_monitor.GetMultisigMessageAggregator()
-	punishmentAggregator = pool_monitor.GetPunishmentAggregator()
-	wincountZlAggregator = pool_monitor.GetWincountZlAggregator()
+	addressAggregator = monitor.GetAddressAggregator()
+	aggPreNetfeeAggregator = monitor.GetAggPreNetfeeAggregator()
+	aggProNetfeeAggregator = monitor.GetAggProNetfeeAggregator()
+	blockAggregator = monitor.GetBlockAggregator()
+	finalHeightAggregator = monitor.GetFinalHeightAggregator()
+	minerBlockrewardAggregator = monitor.GetMinerBlockrewardAggregator()
+	minersInfoAggregator = monitor.GetMinersInfoAggregator()
+	minersMinedAggregator = monitor.GetMinersMinedAggregator()
+	multisigMessageAggregator = monitor.GetMultisigMessageAggregator()
+	punishmentAggregator = monitor.GetPunishmentAggregator()
+	wincountZlAggregator = monitor.GetWincountZlAggregator()
 }
