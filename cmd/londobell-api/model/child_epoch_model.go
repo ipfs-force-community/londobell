8fdd89f27b3bcdb7d14b8fe11fb03ea8d239fe08
xianleigirl <1911276652@qq.com>
2022-09-20 13:46:15 +0800

    fix: correct output type of tipset

--- cmd/londobell-api/model/child_epoch_model.go
+++ cmd/londobell-api/model/child_epoch_model.go
@@ -2,12 +2,11 @@ package model
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"
 )
 
 type ChildEpochRes struct {
 	CurrentEpoch  abi.ChainEpoch `bson:"_id"`
-	CurrentTipset []cid.Cid
+	CurrentTipset []string
 	ChildEpoch    abi.ChainEpoch
-	ChildTipset   []cid.Cid
+	ChildTipset   []string
 }
