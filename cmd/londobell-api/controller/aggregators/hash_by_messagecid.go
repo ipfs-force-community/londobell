06f44504d5af95924ad17d0596b8bb2d30d0588a
xianleigirl <1911276652@qq.com>
2023-05-30 15:32:38 +0800

    feat: add hash_by_messagecid

---
+++ cmd/londobell-api/controller/aggregators/hash_by_messagecid.go
@@ -0,0 +1,37 @@
+package aggregators
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/net/context"
+
+	"github.com/ipfs-force-community/londobell/cmd/londobell-api/model"
+	"github.com/ipfs-force-community/londobell/cmd/londobell-api/util"
+)
+
+func GetHashByMessageCid(c *gin.Context) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	alog := log.With("method", "GetHashByMessageCid")
+	req := model.CommonReq{}
+	res := model.CommonRes{Code: model.Success}
+	err := c.BindJSON(&req)
+	if err != nil {
+		alog.Error(err)
+		util.ReturnOnErr(c, err)
+		return
+	}
+
+	hash, err := GetEthHashByCid(ctx, req.Cid)
+	if err != nil {
+		alog.Error(err)
+		util.ReturnOnErr(c, err)
+		return
+	}
+
+	res.Data = model.HashByMessageCidRes{Hash: hash}
+
+	c.JSON(http.StatusOK, res)
+}
