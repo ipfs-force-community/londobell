5e93bf776a0cc992fc6862aa7ac782182f5a38e1
xianleigirl <1911276652@qq.com>
2023-06-19 16:37:58 +0800

    fix: remove loop

--- cmd/bell/complete_actormessage.go
+++ cmd/bell/complete_actormessage.go
@@ -117,128 +117,125 @@ var completeActorMessageCmd = &cli.Command{
 		log.Infof("begin complete actormessage")
 		starttime := time.Now()
 
-		for {
-			for i := range part {
-				i := i
-				r := part[i]
-				ewg.Go(func() error {
-					if !lim.Acquire(context.TODO()) {
-						return nil
-					}
-
-					defer func() {
-						lim.Release(context.TODO())
-					}()
-
-					pipe, err := aggregators.Parse(model.Ctx{StartEpoch: r.Start, EndEpoch: r.End}, string(js))
-					if err != nil {
-						return err
-					}
-
-					cur, err := traceCol.Aggregate(context.TODO(), pipe)
-					if err != nil {
-						return err
-					}
-
-					var res []ActorMessageRes
-					err = cur.All(context.TODO(), &res)
-					if err != nil {
-						return err
-					}
-
-					actorMessages := make([]ActorMessage, 0)
-					for _, r := range res {
-						storeMap := make(map[address.Address]string)
-
-						fromAddr, err := address.NewFromString(buildnet.NetPrefix + r.From)
-						if err != nil {
-							return err
-						}
-
-						fromID, err := api.StateLookupID(ctx, fromAddr, types.EmptyTSK)
-						if err != nil {
-							fromID = fromAddr
-						}
-
-						storeMap[fromID] = "from"
-
-						toAddr, err := address.NewFromString(buildnet.NetPrefix + r.To)
-						if err != nil {
-							return err
-						}
-
-						toID, err := api.StateLookupID(ctx, toAddr, types.EmptyTSK)
-						if err != nil {
-							toID = toAddr
-						}
-
-						if _, ok := storeMap[toID]; !ok {
-							storeMap[toID] = "to"
-						}
-
-						c, err := cid.Decode(r.Cid)
-						if err != nil {
-							return err
-						}
-
-						var sc cid.Cid
-						sc, _ = cid.Decode(r.SignedCid)
-
-						v, err := big.FromString(r.Value)
-						if err != nil {
-							return err
-						}
-						for actorID, mtype := range storeMap {
-							// todo: insert进去类型是否一致
-							actorMessages = append(actorMessages, ActorMessage{ID: r.ID + "-" + mtype,
-								ActorID:    actorID,
-								Epoch:      abi.ChainEpoch(r.Epoch),
-								Cid:        c,
-								SignedCid:  sc, // null?
-								Value:      v,
-								MethodName: r.MethodName,
-								ExitCode:   exitcode.ExitCode(r.ExitCode),
-								From:       fromAddr,
-								To:         toAddr,
-								IsBlock:    r.IsBlock,
-								Type:       mtype})
-						}
-					}
-
-					var docs []interface{}
-					for _, am := range actorMessages {
-						docs = append(docs, am)
-					}
-
-					total := len(docs)
-					if total > 0 {
-						ires, err := actorMessageCol.InsertMany(ctx, docs, options.InsertMany().SetOrdered(false))
-						if err != nil {
-							if actualErr := extractActualMgoErrors(err); actualErr != nil {
-								return actualErr
-							}
-						}
-
-						log.Infof("part [%v, %v], inserted: %v/%v, elapsed: %v\n", r.Start, r.End, len(ires.InsertedIDs), total, time.Now().Sub(starttime).String())
-						return nil
-					}
-
-					log.Infof("part [%v, %v] total 0, elapsed: %v\n", r.Start, r.End, time.Now().Sub(starttime).String())
+		for i := range part {
+			i := i
+			r := part[i]
+			ewg.Go(func() error {
+				if !lim.Acquire(context.TODO()) {
 					return nil
-				})
-			}
+				}
 
-			//	2500422-2849164
-			//[{2500422 2586822} {2586822 2673222} {2673222 2759622} {2759622 2846022} {2846022 2849164}]
-			//{2500422 2514822} {2514822 2529222} {2529222 2543622} {2543622 2558022} {2558022 2572422} {2572422 2586822} {2586822 2601222} {2601222 2615622} {2615622 2630022} {2630022 2644422} {2644422 2658822} ...
-			if err := ewg.Wait(); err != nil {
-				log.Errorf("falied: %v", err)
-				continue
-			}
+				defer func() {
+					lim.Release(context.TODO())
+				}()
 
-			log.Infof("all finished, elapsed: %v\n", time.Now().Sub(starttime).String())
-			break
+				pipe, err := aggregators.Parse(model.Ctx{StartEpoch: r.Start, EndEpoch: r.End}, string(js))
+				if err != nil {
+					return err
+				}
+
+				cur, err := traceCol.Aggregate(context.TODO(), pipe)
+				if err != nil {
+					return err
+				}
+
+				var res []ActorMessageRes
+				err = cur.All(context.TODO(), &res)
+				if err != nil {
+					return err
+				}
+
+				actorMessages := make([]ActorMessage, 0)
+				for _, r := range res {
+					storeMap := make(map[address.Address]string)
+
+					fromAddr, err := address.NewFromString(buildnet.NetPrefix + r.From)
+					if err != nil {
+						return err
+					}
+
+					fromID, err := api.StateLookupID(ctx, fromAddr, types.EmptyTSK)
+					if err != nil {
+						fromID = fromAddr
+					}
+
+					storeMap[fromID] = "from"
+
+					toAddr, err := address.NewFromString(buildnet.NetPrefix + r.To)
+					if err != nil {
+						return err
+					}
+
+					toID, err := api.StateLookupID(ctx, toAddr, types.EmptyTSK)
+					if err != nil {
+						toID = toAddr
+					}
+
+					if _, ok := storeMap[toID]; !ok {
+						storeMap[toID] = "to"
+					}
+
+					c, err := cid.Decode(r.Cid)
+					if err != nil {
+						return err
+					}
+
+					var sc cid.Cid
+					sc, _ = cid.Decode(r.SignedCid)
+
+					v, err := big.FromString(r.Value)
+					if err != nil {
+						return err
+					}
+					for actorID, mtype := range storeMap {
+						// todo: insert进去类型是否一致
+						actorMessages = append(actorMessages, ActorMessage{ID: r.ID + "-" + mtype,
+							ActorID:    actorID,
+							Epoch:      abi.ChainEpoch(r.Epoch),
+							Cid:        c,
+							SignedCid:  sc, // null?
+							Value:      v,
+							MethodName: r.MethodName,
+							ExitCode:   exitcode.ExitCode(r.ExitCode),
+							From:       fromAddr,
+							To:         toAddr,
+							IsBlock:    r.IsBlock,
+							Type:       mtype})
+					}
+				}
+
+				var docs []interface{}
+				for _, am := range actorMessages {
+					docs = append(docs, am)
+				}
+
+				total := len(docs)
+				if total > 0 {
+					ires, err := actorMessageCol.InsertMany(ctx, docs, options.InsertMany().SetOrdered(false))
+					if err != nil {
+						if actualErr := extractActualMgoErrors(err); actualErr != nil {
+							return actualErr
+						}
+					}
+
+					log.Infof("part [%v, %v], inserted: %v/%v, elapsed: %v\n", r.Start, r.End, len(ires.InsertedIDs), total, time.Now().Sub(starttime).String())
+					return nil
+				}
+
+				log.Infof("part [%v, %v] total 0, elapsed: %v\n", r.Start, r.End, time.Now().Sub(starttime).String())
+				return nil
+			})
 		}
+
+		//	2500422-2849164
+		//[{2500422 2586822} {2586822 2673222} {2673222 2759622} {2759622 2846022} {2846022 2849164}]
+		//{2500422 2514822} {2514822 2529222} {2529222 2543622} {2543622 2558022} {2558022 2572422} {2572422 2586822} {2586822 2601222} {2601222 2615622} {2615622 2630022} {2630022 2644422} {2644422 2658822} ...
+		if err := ewg.Wait(); err != nil {
+			log.Errorf("falied: %v", err)
+			return err
+		}
+
+		log.Infof("all finished, elapsed: %v\n", time.Now().Sub(starttime).String())
 
 		return nil
 	},
