352d1137b343d891d1f7aab81e078398c64c6942
60ke <i505125461@live.com>
2023-12-29 13:13:25 +0800

    feat: add change_actor api

--- cmd/londobell-api/server/server.go
+++ cmd/londobell-api/server/server.go
@@ -287,6 +287,7 @@ func RegisterAggregatorsApi(router *gin.Engine) {
 		// todo: 1. 原来范围请求变成分页请求的接口 2. 原来只请求formal，现在为了实时性也请求tmp 和天佑沟通
 		group.POST("/address", aggregators.GetAddress)
 		group.POST("/actor_state_epoch", aggregators.GetActorStateForEpoch) // todo: account只存一次，head主键不变
+		group.POST("/changed_actors", aggregators.GetChangeActorForEpoch)
 		group.POST("/balance", aggregators.GetBalance)
 		group.POST("/richlist", aggregators.GetRichList)
 		group.POST("/agg_pre_netfee", aggregators.GetAggPreNetFee)
